306df86a1c1a00b2e6ccaed8f8a447a6dd8eb838
dipankar121 <94380559+dipankar121@users.noreply.github.com>
2023-01-24 12:41:40 +0530

    Header

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./App.css";
+import Header from "./components/Header";
 import Login from "./components/Login";
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
+        <Header />
         <Routes>
           <Route exact path="/" element={<Login />} />
         </Routes>
