914fa9178e1a2571cfd7aff6b502a4af32195006
dipankar121 <94380559+dipankar121@users.noreply.github.com>
2023-01-25 01:28:35 +0530

    Home Page

---
+++ src/components/ImgSlider.js
@@ -0,0 +1,11 @@
+import React from "react";
+import styled from "styled-components";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import Slider from "react-slick";
+
+const ImgSlider = (props) => {
+  return <div>ImgSlider</div>;
+};
+
+export default ImgSlider;
